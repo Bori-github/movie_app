1511639e29201bd451cf4bd999f86c0d5743127a
Bori-github <qhflrnfl4324@gmail.com>
2022-12-14 19:20:53 +0900

    refactor(App.js): 라우터 코드 리팩토링
    - react-router-dom 업데이트에 따른 코드 수정

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import React from "react";
-import { HashRouter, Route } from "react-router-dom";
-import Home from "./routes/Home";
-import About from "./routes/About";
-import Detail from "./routes/Detail";
-import Navigation from "./components/Navigation";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './routes/Home';
+import About from './routes/About';
+import Detail from './routes/Detail';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <HashRouter>
+    <Router>
       <Navigation />
-      <Route path="/" exact={true} component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/movie/:id" component={Detail} />
-    </HashRouter>
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/movie/:id" element={<Detail />} />
+      </Routes>
+    </Router>
   );
 }
 
