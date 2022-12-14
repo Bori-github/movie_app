8ec8d66c5039a08be568a96feb65ec104b709a69
Bori-github <qhflrnfl4324@gmail.com>
2022-12-14 19:36:43 +0900

    refactor(Home.js): class 컴포넌트를 function 컴포넌트 코드로 수정

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,54 +1,51 @@
-import React from "react";
-import axios from "axios";
-import Movie from "../components/Movie";
-import "./Home.css";
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import Movie from '../components/Movie';
+import './Home.css';
 
-// react는 자동적으로 class component의 render method를 실행
-class Home extends React.Component {
-  state = {
-    isLoading: true,
-    movies: [],
-  };
-  getMovies = async () => {
+const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
     const {
       data: {
         data: { movies },
       },
     } = await axios.get(
-      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+      'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
     );
-    this.setState({ movies, isLoading: false });
+
+    setMovies(movies);
   };
-  componentDidMount() {
-    this.getMovies();
-  }
-  render() {
-    const { isLoading, movies } = this.state;
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading...</span>
-          </div>
-        ) : (
-          <div className="movies">
-            {movies.map((movie) => (
-              <Movie
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                title_long={movie.title_long}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    );
-  }
-}
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return (
+    <section className="container">
+      {movies && movies.length > 0 ? (
+        <div className="movies">
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              id={movie.id}
+              year={movie.year}
+              title={movie.title}
+              title_long={movie.title_long}
+              summary={movie.summary}
+              poster={movie.medium_cover_image}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      ) : (
+        <div className="loader">
+          <span className="loader__text">Loading...</span>
+        </div>
+      )}
+    </section>
+  );
+};
 
 export default Home;
