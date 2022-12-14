2131cd6d466f43e3fbcbf97007a1bc7f1a5c56e9
Bori-github <qhflrnfl4324@gmail.com>
2022-12-14 19:17:11 +0900

    refactor(index.js): DOM에 리액트 앨리먼트 렌더링 코드 리팩토링
    - 리액트 버전 업데이트에 따른 코드 변경

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+
+ReactDOM.createRoot(container).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
