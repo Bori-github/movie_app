8794b4367ed1d59951b2ac036b37b9e0639199cc
Bori-github <qhflrnfl4324@gmail.com>
2022-12-15 10:55:19 +0900

    refactor(Detail.js): class 컴포넌트 > function 컴포넌트로 수정

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,39 +1,54 @@
-import React from "react";
-import "./Detail.css";
+import axios from 'axios';
+import React, { useCallback, useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import './Detail.css';
 
-class Detail extends React.Component {
-  componentDidMount() {
-    const { location, history } = this.props;
-    if (location.state === undefined) {
-      history.push("/");
-    }
+const Detail = () => {
+  const { id } = useParams();
+  const [movie, setMovie] = useState({});
+
+  const getMovie = useCallback(async () => {
+    const {
+      data: {
+        data: { movie },
+      },
+    } = await axios.get(
+      `https://yts.mx/api/v2/movie_details.json?movie_id=${id}`
+    );
+
+    setMovie(movie);
+  }, [id]);
+
+  useEffect(() => {
+    getMovie();
+  }, [getMovie]);
+
+  if (!movie.id) {
+    return (
+      <div className="loader">
+        <span className="loader__text">Loading...</span>
+      </div>
+    );
   }
-  render() {
-    // console.log(this.props);
-    const { location } = this.props;
-    if (location.state) {
-      return (
-        <div className="detail__container">
-          <img
-            src={location.state.poster}
-            alt={location.state.title}
-            title={location.state.title}
-          />
-          <div className="detail__info">
-            <h3 className="detail__title">{location.state.title_long}</h3>
-            <ul className="detail__genres">
-              {location.state.genres.map((genre, index) => (
-                <li key={index}>{genre}</li>
-              ))}
-            </ul>
-            <p className="detail__summary">{location.state.summary}</p>
-          </div>
-        </div>
-      );
-    } else {
-      return null;
-    }
-  }
-}
+
+  return (
+    <div className="detail__container">
+      <img
+        src={movie.large_cover_image}
+        alt={movie.title}
+        title={movie.title}
+      />
+      <div className="detail__info">
+        <h3 className="detail__title">{movie.title_long}</h3>
+        <ul className="detail__genres">
+          {movie.genres.map((genre, index) => (
+            <li key={index}>{genre}</li>
+          ))}
+        </ul>
+        <p className="detail__summary">{movie.description_full}</p>
+      </div>
+    </div>
+  );
+};
 
 export default Detail;
